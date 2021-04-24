b1ee7a0c76b53c049be87deebb014e4f70a15b90
Tyler Reaves <tylersi93@gmail.com>
2021-04-24 08:59:18 -0400

    Add db

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,15 +1,19 @@
-// Fake users data
-const users = [
-  { id: 1 },
-  { id: 2 },
-  { id: 3 },
-  { id: 4 },
-  { id: 5 },
-  { id: 6 },
-  { id: 67 },
-];
+import User from "./UserModel";
 
-export default function handler(_req, res) {
+// Fake users data
+// const users = [
+//   { id: 1 },
+//   { id: 2 },
+//   { id: 3 },
+//   { id: 4 },
+//   { id: 5 },
+//   { id: 6 },
+//   { id: 67 },
+// ];
+
+export default async function handler(_req, res) {
   // Get data from your database
+  // res.status(200).json(users);
+  const users = await User.find({});
   res.status(200).json(users);
 }
