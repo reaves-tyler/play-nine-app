80b98b5e35187b6b0e427f69af5237c10cee679b
reaves-tyler <tylersi93@gmail.com>
2021-07-24 19:26:44 -0400

    Add Delete Game

--- src/pages/api/game/[id].tsx
+++ src/pages/api/game/[id].tsx
@@ -15,6 +15,13 @@ const handler = async (req, res) => {
 
     return res.status(200).send(game);
   }
+
+  if (req.method === "DELETE") {
+    const { id } = req.query;
+    const game = await Game.deleteOne({ _id: id });
+
+    return res.status(200).send(game);
+  }
 };
 
 export default connectDB(handler);
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,4 +1,4 @@
-import useSwr from "swr";
+import useSwr, { mutate } from "swr";
 import Link from "next/link";
 import {
   Card,
@@ -8,6 +8,7 @@ import {
   Grid,
 } from "@material-ui/core";
 import { Header } from "../../components/Header";
+import axios from "axios";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -17,12 +18,17 @@ export default function games() {
   if (error) return <div>Failed to load games</div>;
   if (!data) return <div>Loading...</div>;
 
+  const endGame = async (e, _id) => {
+    await axios.delete(`/api/game/${_id}`);
+    mutate("/api/game");
+  };
+
   return (
     <>
       <Header />
       <Grid container spacing={3}>
         {data.map((game) => (
-          <Grid item xs={6}>
+          <Grid item xs={6} key={game._id}>
             <Card key={game.id}>
               <CardContent>{game.name}</CardContent>
               <CardActions>
@@ -31,6 +37,13 @@ export default function games() {
                     Join
                   </Button>
                 </Link>
+                <Button
+                  size="large"
+                  color="secondary"
+                  onClick={(e) => endGame(e, game._id)}
+                >
+                  End
+                </Button>
               </CardActions>
             </Card>
           </Grid>
